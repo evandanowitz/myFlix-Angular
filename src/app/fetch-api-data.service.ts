e2efff376d74396e539ac3b6ee387c8afa918b40
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-07-17 16:56:32 -0400

    Create logic for API call for 'Get all movies' endpoint

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -35,3 +35,14 @@ export class FetchApiDataService {
     );
   }
 
+  // Logic for API call for 'Get all movies' endpoint
+  public getAllMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'movies/', {
+      headers: new HttpHeaders({ Authorization: 'Bearer ' + token })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
