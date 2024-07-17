48abc060d3f249545728e1e0e6958205ce19b8d0
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-07-17 16:54:46 -0400

    Initialize FetchApiDataService to handle HTTP requests via API calls

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+
+// Declaring the API URL that will provide data for the client app
+const apiUrl = 'https://myflix-db-movie-app-af5513e7733f.herokuapp.com/'; // Hosted API URL (Heroku)
+
+// A decorator that marks a class as available to be provided and injected as a dependency
+@Injectable({ providedIn: 'root' }) // Configures the service to be provided in the root injector, making it available throughout the app.
+
+export class FetchApiDataService {
+  // This provides HttpClient to entire class, making it available via `this.http`
+  // A special method used for initializing objects created with the class. Called when a new instance of the class is created.
+  // 'private' restricts the visibility of the 'http' property to within the class. 'http' property can only be used inside the FetchApiDataService class.
+  // 'http' is the name of the property that will hold the 'HttpClient' instance.
+  // 'HttpClient' is a service provided by Angular to make HTTP requests. It is part of the @angular/common/http package.
+  // The 'http' property will hold the instance of 'HttpClient' provided by Angular, allowing the class to use it to make HTTP requests.
+  constructor(private http: HttpClient) { }
+
